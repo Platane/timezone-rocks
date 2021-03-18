3296d8bd43bb8fd8c26179386d746906b4a6e1f3
platane <me@platane.me>
2021-03-18 11:31:34 +0100

    feat: timezone manipulation utils

---
+++ src/__tests__/timezone.spec.ts
@@ -0,0 +1,92 @@
+import { getUtcTimestampAt, getDate } from "../timezone";
+
+describe("getDate", () => {
+  it("should get the time for different timezone", () => {
+    const d = new Date("2021-03-18T08:17:22.259Z").getTime();
+
+    expect(getDate("europe/paris", d)).toEqual({
+      year: 2021,
+      month: 3,
+      day: 18,
+      hour: 9 + 17 / 60 + 22 / 3600,
+    });
+
+    expect(getDate("europe/london", d)).toEqual({
+      year: 2021,
+      month: 3,
+      day: 18,
+      hour: 8 + 17 / 60 + 22 / 3600,
+    });
+  });
+
+  it("should get the time before and after dst", () => {
+    // for paris timezone, Sunday, 28 March 2021 — 1 hour forward
+    // at 01:00 +0.0001 it jumps to 02:00 +0.0001
+
+    // before dts leap
+    const d1 = new Date("2021-03-28T00:50:00.000Z").getTime();
+    expect(getDate("europe/paris", d1)).toEqual({
+      year: 2021,
+      month: 3,
+      day: 28,
+      hour: 1 + 50 / 60,
+    });
+
+    // after dts leap
+    const d2 = new Date("2021-03-28T02:10:00.000Z").getTime();
+    expect(getDate("europe/paris", d2)).toEqual({
+      year: 2021,
+      month: 3,
+      day: 28,
+      hour: 4 + 10 / 60,
+    });
+  });
+});
+
+describe("getUtcTimestampAt", () => {
+  it("should get the timestamp for different timezone", () => {
+    expect(
+      new Date(
+        getUtcTimestampAt("europe/paris", {
+          year: 2021,
+          month: 3,
+          day: 18,
+          hour: 9,
+        })
+      )
+    ).toEqual(new Date("2021-03-18T08:00:00.000Z"));
+
+    expect(
+      new Date(
+        getUtcTimestampAt("europe/paris", {
+          year: 2021,
+          month: 3,
+          day: 18,
+          hour: 9.5,
+        })
+      )
+    ).toEqual(new Date("2021-03-18T08:30:00.000Z"));
+
+    expect(
+      new Date(
+        getUtcTimestampAt("europe/paris", {
+          year: 2021,
+          month: 3,
+          day: 28,
+          hour: 2,
+        })
+      )
+    ).toEqual(new Date("2021-03-28T01:00:00.000Z"));
+
+    expect(
+      new Date(
+        getUtcTimestampAt("europe/paris", {
+          year: 2021,
+          month: 3,
+          day: 28,
+          hour: 4,
+        })
+      )
+    ).toEqual(new Date("2021-03-28T02:00:00.000Z"));
+  });
+});
---
+++ src/timezone.tsx
@@ -0,0 +1,40 @@
+import { DateTime } from "luxon";
+
+/**
+ *
+ * @param timezone
+ * @param timestamp timestamp utc
+ */
+export const getDate = (timezone: string, timestamp: number) => {
+  const d = DateTime.fromMillis(timestamp).setZone(timezone);
+
+  return {
+    hour: d.hour + d.minute / 60 + d.second / 3600,
+    day: d.day,
+    month: d.month,
+    year: d.year,
+  };
+};
+
+type Date = {
+  year: number;
+  month: number;
+  day: number;
+  hour: number;
+};
+
+export const getUtcTimestampAt = (timezone: string, date: Date) => {
+  const hour = 0 | date.hour;
+  const minute = (0 | (date.hour * 60)) % 60;
+  const second = (0 | (date.hour * 60 * 60)) % 60;
+  const millisecond = (0 | (date.hour * 60 * 60 * 1000)) % 1000;
+
+  const d = DateTime.fromObject({
+    ...date,
+    hour,
+    minute,
+    second,
+    millisecond,
+  }).setZone(timezone);
+  return d.toMillis();
+};
