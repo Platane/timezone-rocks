e72840db5752ce5fd0d2aed106c03a9098dbde7f
platane <me@platane.me>
2021-04-11 15:08:37 +0200

    🚀redux devtool

--- src/App/store/store.ts
+++ src/App/store/store.ts
@@ -1,4 +1,5 @@
 import create from "zustand";
+import { devtools } from "zustand/middleware";
 import type { Location } from "../../locations/getLocations";
 import { init } from "./persist";
 
@@ -32,59 +33,64 @@ const day = 24 * 60 * 60 * 1000;
 
 const w = Math.max(2.3, Math.min(3, window.innerWidth / 300));
 
-export const useStore = create<State & Api>((set) => ({
-  t,
-  now: t,
-  tWindow: [t - (day * w) / 2, t + (day * w) / 2],
-  locations: [],
-  locationStoreReady: false,
-  dateCursorDragged: false,
-  searchFocused: false,
-  earthReady: false,
-  selectedLocation: null,
+export const useStore = create<State & Api>(
+  devtools((set) => ({
+    t,
+    now: t,
+    tWindow: [t - (day * w) / 2, t + (day * w) / 2],
+    locations: [],
+    locationStoreReady: false,
+    dateCursorDragged: false,
+    searchFocused: false,
+    earthReady: false,
+    selectedLocation: null,
 
-  setTWindowOrigin: (t) =>
-    set(({ tWindow: [a, b] }) => ({
-      t,
-      tWindow: [t - (b - a) / 2, t + (b - a) / 2],
-    })),
-  setT: (t) => set({ t }),
-  initLocations: (locations, t) =>
-    set((s) => ({
-      t: t ?? s.now,
-      locations,
-      locationStoreReady: true,
-      selectedLocation: [locations[0], 1],
-    })),
-  onEarthReady: () => set({ earthReady: true }),
-  addLocation: (location) =>
-    set((s) => ({
-      selectedLocation: [location, (s.selectedLocation?.[1] ?? 1) + 1],
-      locations: [
-        location,
-        ...s.locations.filter((l) => l.key !== location.key),
-      ],
-    })),
-  removeLocation: (location) =>
-    set((s) => ({
-      selectedLocation:
-        s.selectedLocation?.[0] === location ? null : s.selectedLocation,
-      locations: s.locations.filter((l) => l.key !== location.key),
-    })),
+    setTWindowOrigin: (t) =>
+      set(({ tWindow: [a, b] }) => ({
+        t,
+        tWindow: [t - (b - a) / 2, t + (b - a) / 2],
+      })),
+    setT: (t) => set({ t }),
+    initLocations: (locations, t) =>
+      set((s) => ({
+        t: t ?? s.now,
+        locations,
+        locationStoreReady: true,
+        selectedLocation: [locations[0], 1],
+      })),
+    onEarthReady: () => set({ earthReady: true }),
+    addLocation: (location) =>
+      set((s) => ({
+        selectedLocation: [location, (s.selectedLocation?.[1] ?? 1) + 1],
+        locations: [
+          location,
+          ...s.locations.filter((l) => l.key !== location.key),
+        ],
+      })),
+    removeLocation: (location) =>
+      set((s) => ({
+        selectedLocation:
+          s.selectedLocation?.[0] === location ? null : s.selectedLocation,
+        locations: s.locations.filter((l) => l.key !== location.key),
+      })),
 
-  selectLocation: (selectedLocation) =>
-    set((s) => ({
-      selectedLocation: [selectedLocation, (s.selectedLocation?.[1] ?? 1) + 1],
-    })),
-  focusSearch: () => set({ searchFocused: true }),
-  blurSearch: () => set({ searchFocused: false }),
-  startDragDateCursor: () => set({ dateCursorDragged: true }),
-  endDragDateCursor: () =>
-    set((s) => {
-      const r = 15 * 60 * 1000;
-      const t = Math.round(s.t / r) * r;
-      return { dateCursorDragged: false, t };
-    }),
-}));
+    selectLocation: (selectedLocation) =>
+      set((s) => ({
+        selectedLocation: [
+          selectedLocation,
+          (s.selectedLocation?.[1] ?? 1) + 1,
+        ],
+      })),
+    focusSearch: () => set({ searchFocused: true }),
+    blurSearch: () => set({ searchFocused: false }),
+    startDragDateCursor: () => set({ dateCursorDragged: true }),
+    endDragDateCursor: () =>
+      set((s) => {
+        const r = 15 * 60 * 1000;
+        const t = Math.round(s.t / r) * r;
+        return { dateCursorDragged: false, t };
+      }),
+  }))
+);
 
 init(useStore);
