185c049780bdedc290e9f86ac85eef3bf81132e9
platane <me@platane.me>
2021-04-11 14:27:43 +0200

    🚀add icons

--- webpack.config.ts
+++ webpack.config.ts
@@ -70,6 +70,10 @@ const webpackConfiguration: WebpackConfiguration = {
             <meta name="viewport" content="width=device-width,initial-scale=1">
             <title>${htmlWebpackPlugin.options.title}</title>
             <link rel="manifest" href="/manifest.json">
+            <link rel="apple-touch-icon" sizes="180x180" href="/icon-180x180.png">
+            <meta name="theme-color" content="#203b53"/>
+            <link rel="icon" type="image/png" sizes="32x32" href="/icon-32x32.png">
+            <link rel="icon" type="image/png" sizes="16x16" href="/icon-16x16.png">
           </head>
           <body>
             
