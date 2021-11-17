41426bd3cd4b08ba6fbfe2ba39c6d54e97533f1b
platane <me@platane.me>
2021-11-17 17:40:05 +0100

    👷

--- webpack.config.ts
+++ webpack.config.ts
@@ -17,7 +17,7 @@ const webpackConfiguration: WebpackConfiguration = {
   mode,
   devtool: false,
   entry: path.join(__dirname, "src/index"),
-  output: { filename: "[contenthash].js" },
+  output: { filename: "[contenthash].js", clean: true },
   resolve: { extensions: [".tsx", ".ts", ".js"] },
   optimization: {
     minimize: mode === "production",
