1aadda3f2cb68087b3be6c1cf148bed17678473a
platane <me@platane.me>
2021-11-22 18:58:34 +0100

    🚀 cheap avatar fallback

--- src/App/store/selector.ts
+++ src/App/store/selector.ts
@@ -1,22 +1,7 @@
 import type { State } from "./store";
-import { createSelector } from "reselect";
-
-export const currentTimeZone =
-  window.Intl?.DateTimeFormat?.().resolvedOptions?.().timeZone ??
-  "Europe/Stockholm";
 
 export const selectT = (s: State) => s.t;
 
 export const selectTWindow = (s: State) => s.tWindow;
 
-const hour = 1000 * 60 * 60;
-const day = hour * 24;
-
-export const selectHour = createSelector(selectT, (t) => (t % day) / hour);
-
-export const selectTWindowLerp = createSelector(
-  selectTWindow,
-  ([a, b]) =>
-    (x: number) =>
-      (x - a) / (b - a)
-);
+export const selectUseCheapAvatar = () => false;
