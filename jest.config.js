f15078576dde4d95d576282d97c9d7a97a0f8383
platane <me@platane.me>
2022-03-31 11:12:59 +0200

    .

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
   testEnvironment: "node",
   testMatch: ["**/__tests__/**/?(*.)+(spec|test).ts"],
   transform: {
-    "\\.(jsx|ts|tsx)$": "@sucrase/jest-plugin",
+    "\\.(js|jsx|ts|tsx)$": "@sucrase/jest-plugin",
+    "\\.csv$": "jest-raw-loader",
   },
 };
